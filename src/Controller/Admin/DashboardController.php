eaaad0cb0cc9639844e440aab8e40c3cbf79fc1b
Vsevolod <ash.seva@gmail.com>
2024-10-31 03:12:15 +0400

    updated DashboardController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Advertisement;
-use App\Entity\Category;
-use App\Entity\Floor;
-use App\Entity\Renter;
-use App\Entity\StandbyMode;
-use App\Entity\TerminalUpdate;
-use App\Entity\User;
+use App\Entity\Advertisement\Advertisement;
+use App\Entity\Category\Category;
+use App\Entity\Exception;
+use App\Entity\Floor\Floor;
+use App\Entity\Renter\Renter;
+use App\Entity\StandbyMode\StandbyMode;
+use App\Entity\Terminal\Terminal;
+use App\Entity\TerminalUpdate\TerminalUpdate;
+use App\Entity\User\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -18,6 +20,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Service\Attribute\Required;
+use App\Controller\Admin\Category\CategoryCrudController;
 
 
 #[IsGranted("ROLE_ADMIN")]
@@ -30,9 +33,9 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         $url = $this->adminUrlGenerator
-            ->setController(UserCrudController::class)
-            ->setAction(Action::INDEX)
-            ->generateUrl();
+                        ->setController(CategoryCrudController::class)
+                        ->setAction(Action::INDEX)
+                        ->generateUrl();
         return $this->redirect($url);
     }
 
@@ -49,7 +52,9 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Категории', 'fa-solid fa-tag', Category::class);
         yield MenuItem::linkToCrud('Арендаторы', 'fa-solid fa-users', Renter::class);
 
+
         yield MenuItem::section('Карта');
+        yield MenuItem::linkToCrud('Терминалы', 'fa-solid fa-terminal', Terminal::class);
         yield MenuItem::linkToCrud('Режим ожидания', 'fa-solid fa-tv', StandbyMode::class);
         yield MenuItem::linkToCrud('Реклама', 'fa-solid fa-rectangle-ad', Advertisement::class);
         yield MenuItem::linkToCrud('Этажи', 'fa-solid fa-stairs', Floor::class);
@@ -57,13 +62,12 @@ class DashboardController extends AbstractDashboardController
 
         yield MenuItem::section('Софт');
         yield MenuItem::linkToCrud('Обновления', 'fa-solid fa-list-ul', TerminalUpdate::class);
+        yield MenuItem::linkToCrud('Исключения', 'fa-solid fa-bug', Exception::class);
 
 
         yield MenuItem::section('Настройки');
-        yield MenuItem::linkToCrud('Пользователи', 'fa-solid fa-user-gear', User::class)
-                    ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Пользователи', 'fa-solid fa-user-gear', User::class);
         yield MenuItem::linkToUrl('API', 'fa fa-link', 'api')
-                    ->setLinkTarget('_blank')
-                    ->setPermission('ROLE_ADMIN');
+                        ->setLinkTarget('_blank');
     }
 }
